c1cdb7149e54758d04ac3f57b6590481de3168ff
davismiler <0xaliasrowyan@gmail.com>
2024-11-13 08:55:32 +0530

    Boilerplate: Category Settings Component Added

---
+++ src/app/comps/category-settings/category-settings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-settings',
+  standalone: true,
+  imports: [],
+  templateUrl: './category-settings.component.html',
+  styleUrl: './category-settings.component.css'
+})
+export class CategorySettingsComponent {
+
+}
