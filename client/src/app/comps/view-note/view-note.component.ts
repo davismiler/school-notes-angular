fa332d3bd179447c39483cbac3202e6bf6718ee7
davismiler <0xaliasrowyan@gmail.com>
2024-11-22 08:40:43 +0530

    getSubjectByNoteID on View Note

--- client/src/app/comps/view-note/view-note.component.ts
+++ client/src/app/comps/view-note/view-note.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, RouterLink } from "@angular/router";
 import { noteService } from "../../core/services/note.service";
 import { NotecardInterface } from "../../core/interfaces/notecard-interface";
+import { SubjectInterface } from "../../core/interfaces/subject-interface";
 
 @Component({
   selector: "app-view-note",
@@ -18,11 +19,25 @@ export class ViewNoteComponent implements OnInit {
     private route: ActivatedRoute
   ) {}
 
+  noteSubject: SubjectInterface = {
+    id: 0,
+    name: "",
+    color: "",
+    isEditing: false,
+  };
+
   ngOnInit(): void {
     // Get Note Details
     const noteId = Number(this.route.snapshot.params["id"]);
     this.noteService.getNoteById(noteId).then((note: NotecardInterface[]) => {
       this.note = note[0];
     });
+
+    // Get Note SUbject by Note Id
+    this.noteService
+      .getSubjectByNoteID(noteId)
+      .then((noteSubject: SubjectInterface[]) => {
+        this.noteSubject = noteSubject[0];
+      });
   }
 }
