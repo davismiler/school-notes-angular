54b63d88ea3983f3ca00e26ebcc087e6910a1ba4
davismiler <0xaliasrowyan@gmail.com>
2024-11-24 15:48:22 +0530

    Removed Unnecessary Commented code

--- server/db/connection.js
+++ server/db/connection.js
@@ -25,29 +25,3 @@ module.exports = {
   subjectsCollection,
   connectToDatabase,
 };
-
-// async function run() {
-//   try {
-//     // Connect to the "insertDB" database and access its collections
-//     const client = new MongoClient(URI);
-//     const database = client.db("schoolnotes");
-
-//     const notesCollection = database.collection("notes");
-//     const subjectCollection = database.collection("subject");
-
-//     // Create a document to insert
-//     const doc = {
-//       title: "Record of a Shriveled Datum",
-//       content: "No bytes, no problem. Just insert a document, in MongoDB",
-//     };
-//     // Insert the defined document into the "haiku" collection
-//     const result = await notesCollection.insertOne(doc);
-//     // Print the ID of the inserted document
-//     console.log(`A document was inserted with the _id: ${result.insertedId}`);
-//   } finally {
-//     // Close the MongoDB client connection
-//     await client.close();
-//   }
-// }
-// // Run the function and handle any errors
-// run().catch(console.dir);
--- server/routes/subjects.js
+++ server/routes/subjects.js
@@ -24,15 +24,6 @@ router
 
 router
   .route("/:id")
-  // Get Subject by Id
-  // .get(async (req, res) => {
-  //   const subjectID = Number(req.params.id);
-  //   const result = await subjectsCollection
-  //     .find({ id: subjectID }, { projection: { _id: 0 } })
-  //     .toArray();
-
-  //   res.json(result);
-  // })
 
   // Update Subject By ID
   .patch(async (req, res) => {
@@ -59,21 +50,5 @@ router
     res.json({ deleted: true });
     // console.log(result);
   });
-
-// router.get("/getSubjectByNoteID/:id", async (req, res) => {
-// const noteID = Number(req.params.id);
-
-// let noteSubjectId = await notesCollection
-//   .find({ id: noteID }, { projection: { _id: 0, subject_id: 1 } })
-//   .toArray();
-
-// noteSubjectId = noteSubjectId[0].subject_id;
-
-// const subjectData = await subjectsCollection
-//   .find({ id: noteSubjectId }, { projection: { _id: 0 } })
-//   .toArray();
-
-// res.json(subjectData);
-// });
 
 module.exports = router;
